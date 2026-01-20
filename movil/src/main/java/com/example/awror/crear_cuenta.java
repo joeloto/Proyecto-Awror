59cc008fb98b4fd39320368d013a984b1cb9e22e
Hornet <hornet@pharloom.com>
2026-01-20 13:46:22 +0100

    movil2

---
+++ movil/src/main/java/com/example/awror/crear_cuenta.java
@@ -0,0 +1,50 @@
+package com.example.awror;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class crear_cuenta extends AppCompatActivity {
+
+    Button btnCuenta;
+    Button btnCrear;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.crear_cuenta);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnCrear = findViewById(R.id.btnCrear);
+        btnCuenta = findViewById(R.id.btnCuenta);
+
+        btnCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(crear_cuenta.this, inicio_sesion.class);
+                startActivity(i);
+            }
+        });
+
+        btnCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(crear_cuenta.this, cuenta_creada.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
