ae4aecc350d04afcbd1e4ad3730925eeec1fca51
Hornet <hornet@pharloom.com>
2026-02-03 12:22:19 +0100

    web2

--- web/index.php
+++ web/index.php
@@ -1,16 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crear cuenta</title>
     <style>
-        body{
-            background-image: url('https://es.pinterest.com/pin/372813675426052917/');
+        body {
+            background-image: url('https://images.alphacoders.com/130/1307447.jpg');
+            background-size: cover;
+        }
+
+        input {
+            width: 450px;
+            height: 50px;
+            border-radius: 10px;
+            font-size: 20px;
+        }
+
+        section{
+            display: flex;
+            align-content: center;
+            justify-content: center;
         }
     </style>
 </head>
+
 <body>
-    
+    <div>
+        <section>
+            <form action="#">
+                <input type="text" name="nombre" id="nombre" placeholder="Nombre"><br><br>
+                <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos"><br><br>
+                <input type="text" name="email" id="email" placeholder="Email"><br><br>
+                <input type="text" name="password" id="password" placeholder="Contraseña"><br><br>
+                <input type="text" name="usuario" id="usuario" placeholder="Nombre de usuario"><br><br>
+            </form>
+        </section>
+    </div>
 </body>
+
 </html>
