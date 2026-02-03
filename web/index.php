006d12ce0e56dd5b8213f9ed489561321cd8d45d
Hornet <hornet@pharloom.com>
2026-02-03 10:14:29 +0100

    web1

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear cuenta</title>
+    <style>
+        body{
+            background-image: url('https://es.pinterest.com/pin/372813675426052917/');
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
